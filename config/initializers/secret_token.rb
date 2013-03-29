c0a3c357b9b316982070e5d53146381b874d36cc
Brian McDevitt <brian.p.mcdevitt@gmail.com>
2013-03-29 14:34:15 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookClubVotingBooth::Application.config.secret_token = '711e0723fd7c4835834d3fd6733691caa3dfc8a2e93f82a71a9fd15a82f5be0f6dce6ac4867779cc44fb81e2a8efe43bc60b1d07277d218608bbe3be7de2b4fc'
